b45175593713366c2d78446d14ab65f7e94e0772
Jorge Oliveira <jorgeallancastro@icloud.com>
2020-07-16 19:12:11 -0300

    Dropdown in home screen

--- lib/Pages/Home/index.dart
+++ lib/Pages/Home/index.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/Services/Authentication/index.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -32,6 +33,8 @@ class _HomeState extends State<Home> {
     }
   }
 
+  String uf = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,9 +42,10 @@ class _HomeState extends State<Home> {
         brightness: Brightness.light,
         backgroundColor: Color(0xFFF5FFFA),
         elevation: 0,
-        title: Container(
-            width: 40,
-            child: Image.asset("assets/images/simbolo-consulta.png")),
+        /*title: Container(
+          width: 40,
+          child: Image.asset("assets/images/simbolo-consulta.png"),
+        ), */
         leading: IconButton(
           icon: Icon(
             Icons.exit_to_app,
@@ -53,8 +57,209 @@ class _HomeState extends State<Home> {
         ),
       ),
       backgroundColor: Color(0xFFF5FFFA),
-      body: Container(
-        child: Center(child: Text(_getUsername())),
+      body: Padding(
+        padding: EdgeInsets.only(left: 30, top: 50, right: 30, bottom: 60),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              constraints: BoxConstraints(maxWidth: 300),
+              child: Text(
+                "Reivente seu jeito de agendar consultas",
+                style: GoogleFonts.ubuntu(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFF322153),
+                ),
+              ),
+            ),
+            Container(
+              constraints: BoxConstraints(maxWidth: 300),
+              margin: EdgeInsets.only(top: 16),
+              child: Text(
+                "Busque profissionais por especialidade, localização, preço ou nota de avaliação.",
+                style: GoogleFonts.roboto(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  height: 1.5,
+                  color: Color(0xFF6C6C80),
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              margin: EdgeInsets.only(top: 40),
+              child: DropdownButtonFormField(
+                decoration: InputDecoration(
+                  hintText: "Estado",
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  hintStyle: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFFA0A5BD),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.language,
+                    color: Colors.grey,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFFF7F7F7),
+                ),
+                items: ['MG', 'RJ', 'SP'].map((value) {
+                  return DropdownMenuItem(child: Text(value), value: value);
+                }).toList(),
+                onChanged: (String value) {
+                  setState(() {
+                    uf = value;
+                  });
+                },
+              ),
+            ),
+            Container(
+              width: 300,
+              margin: EdgeInsets.only(top: 10),
+              child: DropdownButtonFormField(
+                decoration: InputDecoration(
+                  hintText: "Cidade",
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  hintStyle: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFFA0A5BD),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.home,
+                    color: Colors.grey,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFFF7F7F7),
+                ),
+                items: ['MG', 'RJ', 'SP'].map((value) {
+                  return DropdownMenuItem(child: Text(value), value: value);
+                }).toList(),
+                onChanged: (String value) {
+                  setState(() {
+                    uf = value;
+                  });
+                },
+              ),
+            ),
+            Container(
+              width: 300,
+              margin: EdgeInsets.only(top: 10),
+              child: DropdownButtonFormField(
+                decoration: InputDecoration(
+                  hintText: "Especialidade",
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  hintStyle: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFFA0A5BD),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(
+                      color: Color(0xFFE0E0E0),
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.person_pin,
+                    color: Colors.grey,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFFF7F7F7),
+                ),
+                items: ['MG', 'RJ', 'SP'].map((value) {
+                  return DropdownMenuItem(child: Text(value), value: value);
+                }).toList(),
+                onChanged: (String value) {
+                  setState(() {
+                    uf = value;
+                  });
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
